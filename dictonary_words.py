3acbdb137b9cca77c0bc098b8bad7e47aefcaa85
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-02-06 08:35:05 -0800

    updated

--- dictonary_words.py
+++ dictonary_words.py
@@ -1,26 +1,52 @@
 from random import randint
+from sys import argv
 
-"Import the rearrange words function from the rearrange file"
+"""Import the rearrange words function from the rearrange file
+"""
 from rearrange import rearrange_words
 
-"Store the file with all of the words into a variable"
-filename = "/usr/share/dict/words"
+def dictonary_words(num_words):
 
-"open the file stored in the variable and"
-my_file = open(filename,"r")
+    """Store the file with all of the words into a variable
+    """
+    # filename = "/usr/share/dict/words"
+    filename = "/Users/tylerholland/documents/code/cs1.2/tweet-generator/source_text.txt"
 
-"Read the lines in the variable"
-lines = my_file.readlines()
+    """open the file stored in the variable and
+    """
+    my_file = open(filename,"r") 
 
-"Close the file"
-my_file.close()
-print(lines)
+    """Read the lines in the variable
+    """
+    lines = my_file.readlines()
+
+    word_histogram = {}
+
+    for word in lines:
+
+        word = word.rstrip()
+        word_histogram[word] = word_histogram.get(word, 0) + 1
+
+    print(word_histogram)
+
+    """Close the file
+    """
+    my_file.close()
+    # print(lines)
+
+    """select a random set of words from the file and store in a data type
+    """
+    for line in range(num_words):
+        randomize = randint(0, len(lines))
+        
+        print(lines[randomize])
 
 
-"select a random set of words from the file and store in a data type"
 
-
-"put the number of words requested together into a sentence"
-
-
-"output your sentence"
+if __name__ == "__main__":
+    # print(sentence)
+    args = argv[1]
+    num_words = int(args)
+    dictonary_words(num_words)
+    print(num_words)
+    print(args)
---
+++ histogram.py
@@ -0,0 +1,53 @@
+
+
+
+
+
+def get_text_words(file):
+    '''opens text file then puts it into an array of strings
+    '''
+    f = open(file, "r")
+    lines = f.readlines()
+    f.close()
+    words = []
+    for line in lines:
+        for word in line.split():
+            words.append(word)
+    return words
+
+def histogram(text_file):
+    ''' return a histogram data structure that stores each unique word along with the number of times the word appears in the source text
+    '''
+    histogram = dict()
+    for word in text_file:
+        if word not in histogram:
+            histogram[word] = 1
+        else:
+            histogram[word] += 1
+    return histogram
+
+def repeated_words(histogram):
+    '''returns a list of repeated words
+    '''
+    list_repeated_words = []
+    for word, count in histogram.items():
+        if count > 1:
+            list_repeated_words.append(word)
+    return list_repeated_words
+
+def unique_words(histogram):
+    ''' returns the total count of unique words in the histogram
+    '''
+    return len(histogram)
+
+def frequency(word, histogram):
+    '''returns the number of times that a word appears in a text.
+    '''
+    return histogram[word]
+
+if __name__ == '__main__':
+    words = get_text_words('source_text.txt')
+    histogram = histogram(words)
+    print(repeated_words(histogram))
+    print(unique_words(histogram))
+    print(frequency("of", histogram))
